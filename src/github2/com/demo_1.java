4cee28761b248b47dd8948d4c43461209b99cd31
User <User@DESKTOP-SD7MD3K>
2022-10-31 02:38:15 +0530

    first commit

---
+++ src/github2/com/demo_1.java
@@ -0,0 +1,23 @@
+package github2.com;
+
+
+
+public class demo_1 {
+	
+	
+	public static void main(String[] args) {
+		
+		m1();
+		
+		
+	}
+
+	
+	
+	public static  void m1() {
+		
+		System.out.println("hello");
+		
+		
+	}
+}
